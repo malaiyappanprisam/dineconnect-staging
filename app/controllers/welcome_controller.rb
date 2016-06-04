b448eb21f2a5e121d4aac32135bbd0134f7b70cc
Tito Pandu Brahmanto <tito@pandubrahmanto.com>
2016-06-04 22:55:19 +0700

    [#120893743 #120893723] Some customisation on backend

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @users = User.page(params[:page])
   end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,9 +3,12 @@ require "rails_helper"
 describe WelcomeController do
   describe "GET /index" do
     it "returns http response :ok" do
+      create_list :user, 2
+
       get :index
 
       expect(response).to have_http_status(:ok)
+      expect(assigns(:users)).to be_present
     end
   end
 end
