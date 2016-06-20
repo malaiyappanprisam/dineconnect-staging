6771289fc85c1edf17c957b3ed56f7dda9f02fc3
Tito Pandu Brahmanto <tito@pandubrahmanto.com>
2016-06-20 21:19:12 +0700

    Fix sidekiq issue on heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
+require 'sidekiq'
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDIS_URL'], size: 1 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDIS_URL'], size: 9 }
 end
