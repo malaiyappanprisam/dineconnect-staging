272684ba9bd90dc4604bc7b130e33590278c741a
Tito Pandu Brahmanto <tito@pandubrahmanto.com>
2016-07-05 13:03:16 +0700

    [#121345079] Add down migration for default role

--- db/migrate/20160704232817_add_default_role_to_users.rb
+++ db/migrate/20160704232817_add_default_role_to_users.rb
@@ -1,7 +1,10 @@
 class AddDefaultRoleToUsers < ActiveRecord::Migration
-  def change
+  def up
     change_column_default(:users, :role, 0)
 
     User.where(role: nil).update_all(role: 0)
+  end
+
+  def down
   end
 end
