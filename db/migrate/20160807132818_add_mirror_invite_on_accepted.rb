45b2112e0d43f11efe96f76e352844efa64da51d
Tito Pandu Brahmanto <tito@pandubrahmanto.com>
2016-08-07 20:37:02 +0700

    [#127863911] Fix migration

--- db/migrate/20160807132818_add_mirror_invite_on_accepted.rb
+++ db/migrate/20160807132818_add_mirror_invite_on_accepted.rb
@@ -1,6 +1,6 @@
 class AddMirrorInviteOnAccepted < ActiveRecord::Migration
   def up
-    Invite.where(status: :accept, initiator: true).each do |invite|
+    Invite.where(status: Invite.statuses[:accept], initiator: true).each do |invite|
       Invite.create!(user: invite.invitee, invitee: invite.user, status: :accept,
                      restaurant: invite.restaurant, payment_preference: invite.payment_preference,
                      initiator: false)
