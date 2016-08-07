a8a6b5d93cbee089f8632043872c9c461e829684
Tito Pandu Brahmanto <tito@pandubrahmanto.com>
2016-08-07 20:31:59 +0700

    [#127863911] Migrate current invite to initiator

---
+++ db/migrate/20160807132818_add_mirror_invite_on_accepted.rb
@@ -0,0 +1,12 @@
+class AddMirrorInviteOnAccepted < ActiveRecord::Migration
+  def up
+    Invite.where(status: :accept, initiator: true).each do |invite|
+      Invite.create!(user: invite.invitee, invitee: invite.user, status: :accept,
+                     restaurant: invite.restaurant, payment_preference: invite.payment_preference,
+                     initiator: false)
+    end
+  end
+
+  def down
+  end
+end
